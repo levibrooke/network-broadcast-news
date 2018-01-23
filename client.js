f78c89305dc81bbd80bbb98f872f6bee4975bb5b
Levi Porter <levi@levibrooke.com>
2018-01-22 19:58:22 -1000

    Started server up, working on broadcasting data

--- client.js
+++ client.js
@@ -1,1 +1,21 @@
 const net = require(`net`);
+
+// create a new socket connection
+const client = new net.Socket();
+
+client.connect(6969, `0.0.0.0`, () => {
+  console.log(`client connected`);
+
+  // write to server
+  client.write(`Hello, server!`);
+  client.pipe(client);
+});
+
+client.on(`data`, (data) => {
+  console.log(`Received ${data}`);
+  // client.destroy();
+})
+
+client.on(`close`, () => {
+  console.log(`Connection closed`);
+});
--- server.js
+++ server.js
@@ -1,1 +1,29 @@
 const net = require(`net`);
+
+// create a new server
+const server = net.createServer((socket) => {
+
+  // client is listening for connection
+  console.log(`client connected`);
+
+  socket.write(`Hello, socket client!`);
+  socket.pipe(socket);
+
+  socket.on(`data`, (data) => {
+    socket.write(`socket data`);
+  })
+
+  socket.on(`end`, () => {
+    console.log(`client disconnected`);
+  });
+});
+
+server.on(`error`, (err) => {
+  throw err;
+});
+
+server.listen(6969, `0.0.0.0`, () => {
+  console.log(`server listening on port 6969`);
+});
+
+// broadcast
